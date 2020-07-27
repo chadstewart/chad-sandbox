d10f3b5692e62bec5928409e07f93ac36eee082a
Chad Stewart <chadrhonanstewart@gmail.com>
2020-07-27 16:45:28 -0500

    Initial commit for JavaScript Sandbox.

---
+++ javascript/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,28 @@
+for(let i = 1; i <= 100; i++) {
+
+  debugger;
+
+  if(i % 3 === 0 && i % 5 === 0) {
+    
+    console.log('fizzbuzz');
+    continue;
+
+  }
+
+  if(i % 3 === 0) { 
+    
+    console.log('fizz');
+    continue;
+
+  }
+
+  if(i % 5 === 0) {
+    
+    console.log('buzz');
+    continue;
+
+  }
+
+  console.log(i);
+
+}
